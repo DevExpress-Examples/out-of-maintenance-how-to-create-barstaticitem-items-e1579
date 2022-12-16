73937f315b225d2a9f03f48397b769abebf008ff
DevExpressExampleBot <fakeEmail>
2022-12-16 22:39:44 +0400

    Source auto update [skip ci]

--- VB/BarStaticItemEx/Window1.xaml.vb
+++ VB/BarStaticItemEx/Window1.xaml.vb
@@ -1,35 +1,25 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace BarStaticItemEx
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			AddHandler rtfEditor.SelectionChanged, AddressOf rtfEditor_SelectionChanged
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub rtfEditor_SelectionChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim line As Integer = 0
-			Dim col As Integer = 0
-			rtfEditor.CaretPosition.GetLineStartPosition(-100000, line)
-			col = rtfEditor.CaretPosition.GetOffsetToPosition(rtfEditor.CaretPosition.GetLineStartPosition(0))
-			staticItemLine.Content = "Line: " & (-line).ToString() & "  Position: " & (-col).ToString()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            AddHandler Me.rtfEditor.SelectionChanged, New RoutedEventHandler(AddressOf rtfEditor_SelectionChanged)
+        End Sub
+
+        Private Sub rtfEditor_SelectionChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim line As Integer = 0
+            Dim col As Integer = 0
+            Me.rtfEditor.CaretPosition.GetLineStartPosition(-100000, line)
+            col = Me.rtfEditor.CaretPosition.GetOffsetToPosition(Me.rtfEditor.CaretPosition.GetLineStartPosition(0))
+            Me.staticItemLine.Content = "Line: " & (-line).ToString() & "  Position: " & (-col).ToString()
+        End Sub
+    End Class
 End Namespace
